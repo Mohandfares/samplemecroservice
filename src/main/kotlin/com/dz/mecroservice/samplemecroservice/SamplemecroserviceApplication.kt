12e5b2300658646d043a2e80e4617ada5bb33443
mohandfares <farestecweb@gmail.com>
2021-07-23 21:36:20 +0100

    add spring doc with swagger openapi

--- src/main/kotlin/com/dz/mecroservice/samplemecroservice/SamplemecroserviceApplication.kt
+++ src/main/kotlin/com/dz/mecroservice/samplemecroservice/SamplemecroserviceApplication.kt
@@ -1,11 +1,14 @@
 package com.dz.mecroservice.samplemecroservice
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties(BlogProperties::class)
+@OpenAPIDefinition(info = Info(title = "blog API", version = "1.0.0", description = ""))
 class SamplemecroserviceApplication
 
 fun main(args: Array<String>) {
